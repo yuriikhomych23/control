0922c317700cb2db45ae11a34821dfdd9a7fa029
yuriikhomych23 <yuriikhomych94@gmail.com>
2020-03-16 18:54:58 +0000

    Sample code

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 //EDIT
 
-let text = document.getElementById('textBlock')
+let textBlock = document.getElementById('textBlock')
 let cont = document.getElementById('area')
 let btnEdit = document.getElementById('btnEdit')
+let btnAdd = document.getElementById('btnAdd')
 btnEdit.addEventListener('click', function(event) {
     cont.style.display = 'flex';
-    secondBlock.style.display = 'none';
-    cont.innerHTML = text.innerHTML ;
+    cont.innerHTML = textBlock.innerHTML ;
+    btnSave.style.display = 'flex';
+    btnAdd.style.display = 'flex';
 })
 
 
 //SAVE
 let btnSave = document.getElementById('btnSave');
 btnSave.addEventListener('click', function() {
-     text.innerHTML = cont.value;
+     textBlock.innerHTML = cont.value;
+     cont.style.display = 'none';
+     btnSave.style.display = 'none';
+    btnAdd.style.display = 'none';
 })
 
 
